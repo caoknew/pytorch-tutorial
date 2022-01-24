f02cad6aea4e6ce2d3ab382b3ecb18090d342f55
caozhi <caozhicz@mail.ustc.edu.cn>
2022-01-24 13:33:27 +0800

    finish logistic_regression

--- tutorials/01-basics/logistic_regression/main.py
+++ tutorials/01-basics/logistic_regression/main.py
@@ -3,31 +3,30 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 
-
-# Hyper-parameters 
-input_size = 28 * 28    # 784
+# Hyper-parameters
+input_size = 28 * 28  # 784
 num_classes = 10
 num_epochs = 5
 batch_size = 100
 learning_rate = 0.001
 
 # MNIST dataset (images and labels)
-train_dataset = torchvision.datasets.MNIST(root='../../data', 
-                                           train=True, 
+train_dataset = torchvision.datasets.MNIST(root='../../data',
+                                           train=True,
                                            transform=transforms.ToTensor(),
                                            download=True)
 
-test_dataset = torchvision.datasets.MNIST(root='../../data', 
-                                          train=False, 
+test_dataset = torchvision.datasets.MNIST(root='../../data',
+                                          train=False,
                                           transform=transforms.ToTensor())
 
 # Data loader (input pipeline)
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
-                                           batch_size=batch_size, 
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
                                            shuffle=True)
 
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, 
-                                          batch_size=batch_size, 
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
                                           shuffle=False)
 
 # Logistic regression model
@@ -35,28 +34,29 @@ model = nn.Linear(input_size, num_classes)
 
 # Loss and optimizer
 # nn.CrossEntropyLoss() computes softmax internally
-criterion = nn.CrossEntropyLoss()  
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 # Train the model
 total_step = len(train_loader)
 for epoch in range(num_epochs):
+    # enumerate function convert a to (index, a)
     for i, (images, labels) in enumerate(train_loader):
         # Reshape images to (batch_size, input_size)
         images = images.reshape(-1, input_size)
-        
+
         # Forward pass
         outputs = model(images)
         loss = criterion(outputs, labels)
-        
+
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        
-        if (i+1) % 100 == 0:
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}' 
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+        if (i + 1) % 100 == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
 
 # Test the model
 # In test phase, we don't need to compute gradients (for memory efficiency)
@@ -66,6 +66,7 @@ with torch.no_grad():
     for images, labels in test_loader:
         images = images.reshape(-1, input_size)
         outputs = model(images)
+        # returns: item and index
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum()
